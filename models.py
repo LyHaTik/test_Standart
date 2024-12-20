54098b1675945a85f57ce222f561bec06f0ba668
Александр Павлов <LyHaTik12@yandex.ru>
2024-12-20 23:54:27 +0300

    ok

--- models.py
+++ models.py
@@ -58,7 +58,7 @@ class TaskSchedule(db.Model):
     __tablename__ = 'taskschedule'
     
     id = db.Column(db.Integer, primary_key=True)
-    task_name = db.Column(db.String(255), unique=True, nullable=False)
+    task_name = db.Column(db.String(255), default='default_task', nullable=False)
     interval_seconds = db.Column(db.Integer, default=60)
     last_run = db.Column(db.DateTime, nullable=True)
     
